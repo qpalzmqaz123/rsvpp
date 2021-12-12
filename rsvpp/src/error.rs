e4bff061b22c55a4f6c31bf6217074ed4e01970b
wangqj <qpalzmqaz123@gmail.com>
2021-12-12 23:47:25 +0800

    update error

--- rsvpp/src/error.rs
+++ rsvpp/src/error.rs
@@ -1,43 +1,34 @@
-use std::fmt::{Display, Formatter};
-
 macro_rules! lazy_error_impl {
     ($fn:ident, $kind:ident) => {
         pub fn $fn<S: Into<String>>(msg: S) -> Self {
-            Self {
-                kind: ErrorKind::$kind,
-                message: msg.into(),
-            }
+            Self::$kind(msg.into())
         }
     };
 }
 
-#[derive(Debug, Clone)]
-pub enum ErrorKind {
-    Argument,
-    Internal,
-    MsgIdMismatch,
-    CrcMismatch,
-    VppApi,
-    Timeout,
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("IO error: `{0}`")]
+    Io(#[from] std::io::Error),
+    #[error("Unknown error: `{0}`")]
+    Unknown(String),
+    #[error("Internal error: `{0}`")]
+    Internal(String),
+    #[error("Argument error: `{0}`")]
+    Argument(String),
+    #[error("Message id mismatch: `{0}`")]
+    MsgIdMismatch(String),
+    #[error("Crc id mismatch: `{0}`")]
+    CrcMismatch(String),
+    #[error("App api error: `{0}`")]
+    VppApi(String),
+    #[error("Timeout error: `{0}`")]
+    Timeout(String),
 }
-
-#[derive(Debug)]
-pub struct Error {
-    pub kind: ErrorKind,
-    pub message: String,
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl std::error::Error for Error {}
 
 impl Error {
-    lazy_error_impl! {argument, Argument}
     lazy_error_impl! {internal, Internal}
+    lazy_error_impl! {argument, Argument}
     lazy_error_impl! {msg_id_mismatch, MsgIdMismatch}
     lazy_error_impl! {crc_mismatch, CrcMismatch}
     lazy_error_impl! {vpp_api, VppApi}
@@ -46,12 +37,6 @@ impl Error {
 
 impl From<Box<dyn std::error::Error>> for Error {
     fn from(e: Box<dyn std::error::Error>) -> Self {
-        Self::internal(format!("{}", e))
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
         Self::internal(format!("{}", e))
     }
 }
