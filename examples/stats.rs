df000ca3123541a2299325aa33f39aae338940ff
yangsj <yangsj@xnetworks.cn>
2022-02-07 02:21:06 +0000

    support acl stats

--- examples/stats.rs
+++ examples/stats.rs
@@ -2,10 +2,13 @@ use rsvpp::Stats;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let stats = Stats::connect("/var/run/vpp/stats.sock").await?;
+    let stats = Stats::connect("/run/vpp/stats.sock").await?;
 
     let ifaces = stats.interface();
     println!("{:#?}", ifaces);
+
+    let acls = stats.acl();
+    println!("acls{:#?}", acls);
 
     Ok(())
 }
--- rsvpp/src/stats/mod.rs
+++ rsvpp/src/stats/mod.rs
@@ -2,7 +2,7 @@ mod socket;
 mod util;
 mod vpp_types;
 
-use std::ffi::CStr;
+use std::{ffi::CStr, vec};
 
 use libc::{c_char, c_void};
 use socket::Socket;
@@ -16,6 +16,20 @@ pub struct InterfaceStats {
     pub tx_pkts: u64,
     pub rx_bytes: u64,
     pub rx_pkts: u64,
+}
+
+#[derive(Debug, Clone)]
+pub struct AclRuleStats {
+    pub packets: u64,
+    pub bytes: u64,
+
+    pub rule_index: u32,
+}
+
+#[derive(Debug, Clone)]
+pub struct AclStats {
+    acl_index: u32,
+    rule_stats: Vec<AclRuleStats>,
 }
 
 pub struct Stats {
@@ -89,6 +103,48 @@ impl Stats {
                 }
             })
             .collect()
+    }
+
+    pub fn acl(&self) -> Vec<AclStats> {
+        let mut rx_ptr = 0usize;
+        let mut acl_count = 0;
+
+        let mut acl_stats: Vec<AclStats> = vec![];
+        for entry in self.header.segments() {
+            if entry.name_starts_with("/acl") {
+                rx_ptr = unsafe { entry.u.data };
+
+                let rx_ptr =
+                    self.header.adjust_ptr(rx_ptr as *const c_void) as *const *const c_void;
+
+                let rx_ptr = unsafe { self.header.adjust_ptr(*(rx_ptr)) } as *const VlibCounterT;
+                let count = unsafe { util::vec_len(rx_ptr as *const c_void) };
+
+                let acl_rule_stats: Vec<AclRuleStats> = (0..count - 1)
+                    .map(|index| {
+                        let rx = unsafe { &*rx_ptr.offset(index as isize) };
+                        AclRuleStats {
+                            packets: rx.packets,
+                            bytes: rx.bytes,
+
+                            rule_index: index,
+                        }
+                    })
+                    .collect();
+                acl_stats.push(AclStats {
+                    acl_index: acl_count,
+                    rule_stats: acl_rule_stats,
+                });
+
+                acl_count += 1;
+            }
+        }
+
+        if rx_ptr == 0 {
+            return vec![];
+        }
+
+        acl_stats
     }
 }
 
